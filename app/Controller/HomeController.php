ad95f633f2ebd2fa0a17b41127d0605dc607cedb
mrosyidi <rosyidiyuekunchen@gmail.com>
2026-06-21 15:45:04 +0700

    add HomeController.php

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php 
+
+    namespace Cafetaria\Controller;
+
+    class HomeController
+    {
+        function index(): void 
+        {
+            echo "HomeController.index()";
+        }
+
+        function hello(): void 
+        {
+            echo "HelloController.hello()";
+        }
+
+        function world(): void 
+        {
+            echo "HelloController.world()";
+        }
+    }
