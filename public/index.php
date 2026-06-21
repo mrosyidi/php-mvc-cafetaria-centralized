bc8551504c65bf34ffd3e4af6273cf903c92538e
mrosyidi <rosyidiyuekunchen@gmail.com>
2026-06-21 15:46:54 +0700

    update public/index.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
     require_once __DIR__ . '/../vendor/autoload.php';
     
     use Cafetaria\App\Router;
+    use Cafetaria\Controller\HomeController;
 
-    Router::add('GET', '/', 'HomeController', 'index');
-    Router::add('GET', '/login', 'UserController', 'login');
-    Router::add('GET', '/register', 'UserController', 'register');
+    Router::add('GET', '/', HomeController::class, 'index');
+    Router::add('GET', '/hello', HomeController::class, 'hello');
+    Router::add('GET', '/world', HomeController::class, 'world');
     Router::run();
