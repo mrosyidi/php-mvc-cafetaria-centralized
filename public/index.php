5ac32e04f8cd2145ffad3e42e7719e6aeca62e87
mrosyidi <rosyidiyuekunchen@gmail.com>
2026-06-21 16:20:12 +0700

    add ProductController.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@
     
     use Cafetaria\App\Router;
     use Cafetaria\Controller\HomeController;
+    use Cafetaria\Controller\ProductController;
 
+    Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
     Router::add('GET', '/', HomeController::class, 'index');
     Router::add('GET', '/hello', HomeController::class, 'hello');
     Router::add('GET', '/world', HomeController::class, 'world');
